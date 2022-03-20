ba80560851dcfcdea4b48e9c7707312b36c673f4
rodion.gordienko <rodion.gordienko@koroka.solutions>
2022-03-20 15:59:31 +0200

    Code cleaning

--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -4,15 +4,13 @@ import OrderForm from "./OrderForm";
 function OrderModal({dialog, formData}) {
     const modalRef = useRef(null)
     const MODAL_INNER_CLASS = 'modal-window'
-    const [modalState, setModalState] = useState(false)
 
     useEffect(()=>{
         modalRef.current.onclick = handleModalOutsideClick
     },[])
 
     useEffect(()=> {
-        setModalState(dialog)
-        if(!dialog){
+        if(!dialog){ // clean event when modal is closed
             return window.removeEventListener('keydown', handleModalKeydown)
         }
         window.addEventListener('keydown', handleModalKeydown)
@@ -20,18 +18,18 @@ function OrderModal({dialog, formData}) {
 
     function handleModalOutsideClick(e) {
         const {path} = e
+        const clickInside = findElementInPathByClass(MODAL_INNER_CLASS, path)
+        if(!clickInside) closeModal()
+    }
 
-        const clickInside = path.reduce((reducer, current) => {
-            const containsInnerModal = !!current?.classList?.contains(MODAL_INNER_CLASS)
-            if(containsInnerModal){
-                return reducer = containsInnerModal
+    function findElementInPathByClass(elementClass = '', pathArr = []) {
+        return pathArr.reduce((reducer, current) => {
+            const containsClass = !!current?.classList?.contains(elementClass)
+            if(containsClass){
+                return reducer = containsClass
             }
             return reducer
         },false)
-
-        if(!clickInside) {
-            closeModal()
-        }
     }
 
     function closeModal() {
@@ -48,7 +46,7 @@ function OrderModal({dialog, formData}) {
     }
 
     return (
-        <div className={`modal ${modalState ? 'active' : ''}`} ref={modalRef}>
+        <div className={`modal ${dialog ? 'active' : ''}`} ref={modalRef}>
             <div className={`modal__inner ${MODAL_INNER_CLASS}`}>
                 <div className="modal__close" onClick={closeModal}/>
                 <div className="modal__type">{formData?.category}</div>
